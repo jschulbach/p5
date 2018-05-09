54d1e3f42a9f8d0e84231914f54b742a47a5cf39
jschulbach <jschulbach@Joels-MacBook.local>
2018-05-08 20:31:05 -0700

    Test Commit

---
+++ sketch.js
@@ -0,0 +1,33 @@
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	
+
+  w = new Walker();
+  background(255);
+}
+
+function draw() {
+	w.step();
+	w.display();
+}
+
+class Walker {
+	constructor() {
+		this.x = width/2;
+		this.y = height/2;
+	}
+	display() {
+		stroke(0);
+		point(this.x,this.y);
+	}
+	step() {
+		let stepx = Math.random();
+		stepx *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
+		let stepy = Math.random();
+		stepy *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
+
+		this.x += stepx;
+		this.y += stepy;
+		
+	}
+}
